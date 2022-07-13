11bf69e2c5320b8992e6947c107312cda36ccc04
Pedram <pedrambghr@gmail.com>
2022-07-13 21:26:38 +0430

    create Header & ExpenseApp components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
+import ExpenseApp from "./components/expense-app/ExpenseApp";
+import Header from "./components/layouts/Header";
+
 const App = () => {
   return ( 
-    <div className="App"></div>
+    <div className="flex flex-col items-center w-full h-full min-h-screen bg-slate-100">
+      <Header />
+      <ExpenseApp />
+    </div>
    );
 }
  
---
+++ src/components/expense-app/ExpenseApp.jsx
@@ -0,0 +1,9 @@
+const ExpenseApp = () => {
+  return ( 
+    <section>
+      <h1>Expense App</h1>
+    </section>
+   );
+}
+ 
+export default ExpenseApp;
